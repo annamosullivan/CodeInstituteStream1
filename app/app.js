a20f580823d4e29f7a86b5c5b59f0d78fbeeb91e
annamosullivan <osullivan_anne@hotmail.com>
2016-02-24 23:57:05 +0000

    deleted script.js, resized and centered items in flexslider, deleted commented out code in app.js.Tried to amend navbar with amended code to make the menu collapse when item clicked on in mobile view

--- app/app.js
+++ app/app.js
@@ -17,27 +17,3 @@ angular.module('myApp', [
   $routeProvider.otherwise({redirectTo: '/view1'});
 }]);
 
-/*angular.module('myApp').controller('FormCtrl', ['$scope', function($scope) {
-    $scope.formInfo = {};
-    $scope.saveData = function() {
-      $scope.nameRequired = '';
-      $scope.emailRequired = '';
-      $scope.phoneRequired = '';
-      $scope.messageRequired = '';
-
-      if (!$scope.formInfo.Name) {
-        $scope.nameRequired = 'Name Required';
-      }
-
-      if (!$scope.formInfo.Email) {
-        $scope.emailRequired = 'Email Required';
-      }
-
-      if (!$scope.formInfo.Phone) {
-        $scope.phoneRequired = 'Phone Required';
-      }
-      if (!$scope.formInfo.Message) {
-        $scope.passwordRequired = 'Message Required';
-      }
-    };
-}]);*/
